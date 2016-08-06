b09074bd07f994ba6156cf286c916d81662e5bcb
c0re <maddoc46@yahoo.com>
2016-08-06 17:58:32 +0200

    End0fDay Backup

--- DrawingGraphicsConsole01/MyForm.cs
+++ DrawingGraphicsConsole01/MyForm.cs
@@ -7,18 +7,15 @@ using System.Windows.Forms;
 using System.Drawing;
 
 
-
-
-
 namespace DrawingGraphicsConsole01
 {
     sealed class MyForm : Form
     {
         //DECLARE WIDGETS AND COMPONENTS;
         Graphics g;
-        Button btnCalc;
+        Button btnCalc,btnClear;
         Pen greyPen = new Pen(Color.Gray);
-        Pen blackPen = new Pen(Color.Black);
+        Pen redPen = new Pen(Color.Red);
 
         //CONSTRUCTOR
         private MyForm()
@@ -41,23 +38,35 @@ namespace DrawingGraphicsConsole01
 
             //create widgets
             btnCalc = new Button();
-            btnCalc.Location = new Point(700, 510);
+            btnCalc.Location = new Point(700, 410);
             btnCalc.Size = new Size(70, 40);
             btnCalc.Text = "Draw Graph";
             btnCalc.Click += BtnCalc_Click;
             this.Controls.Add(btnCalc);
 
+            btnClear = new Button();
+            btnClear.Text = "Erase Graph";
+            btnClear.Location = new Point(700, 470);
+            btnClear.Size = new Size(70, 40);
+            btnClear.Click += BtnClear_Click;
+            this.Controls.Add(btnClear);
+
             //turn form into a graphics surface
             g = this.CreateGraphics();
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            //Clear the Graph 
+            g.Clear(Color.White);
+            DrawCross();
         }
 
         //EVENTS
         private void MyForm_Shown(object sender, EventArgs e)
         {
-            g.DrawLine(greyPen, new Point(150, 280), new Point(650, 280));
-            g.DrawLine(greyPen, new Point(400, 20), new Point(400, 520));
+            DrawCross();
         }
-
         private void BtnCalc_Click(object sender, EventArgs e)
         {
             //Pushing the button will draw a graph on 
@@ -65,9 +74,16 @@ namespace DrawingGraphicsConsole01
 
             for (int x = -100; x < 100; x++)
             {
-                int y = (int)Math.Pow(x, 2) - (4 * x) + 2;
-                g.DrawLine(blackPen,new Point(zeroPoint.X +x, zeroPoint.Y ), new Point(zeroPoint.X +x, zeroPoint.Y - y));
+                int y = (int)(2 * Math.Pow(x, 2)) + (5 * x) + 20;
+                g.DrawLine(redPen,new Point(zeroPoint.X +x, zeroPoint.Y - y + 3), new Point(zeroPoint.X +x, zeroPoint.Y - y));                
             }
+        }
+
+        //HELPER METHODS
+        private void DrawCross()
+        {
+            g.DrawLine(greyPen, new Point(150, 280), new Point(650, 280));
+            g.DrawLine(greyPen, new Point(400, 20), new Point(400, 520));
         }
 
     }
