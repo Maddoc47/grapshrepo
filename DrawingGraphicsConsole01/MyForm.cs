287f363cd301644ad682fdd04d0130cf57145942
c0re <maddoc46@yahoo.com>
2016-08-06 17:24:01 +0200

    Graph Working

--- DrawingGraphicsConsole01/MyForm.cs
+++ DrawingGraphicsConsole01/MyForm.cs
@@ -7,61 +7,68 @@ using System.Windows.Forms;
 using System.Drawing;
 
 
+
+
+
 namespace DrawingGraphicsConsole01
 {
-    class MyForm : Form
+    sealed class MyForm : Form
     {
-        //DECLARE WIDGETS;
+        //DECLARE WIDGETS AND COMPONENTS;
         Graphics g;
         Button btnCalc;
-        int x1 = 10;
-        int y1 = 10;
-        int x2 = 400;
-        int y2 = 400;
+        Pen greyPen = new Pen(Color.Gray);
+        Pen blackPen = new Pen(Color.Black);
 
+        //CONSTRUCTOR
         private MyForm()
-        {            
+        {
         }
         public MyForm(string title = "")
         {
-
-            InitForm(title);            
-            InitGraphics();
+            InitForm(title);
         }
 
+        //INIT METHODS
         private void InitForm(string title)
         {
+            //set form layout
             this.Text = title;
             this.Width = 800;
             this.Height = 600;
             this.BackColor = Color.White;
+            this.Shown += MyForm_Shown;
 
-            //CREATE WIDGETS
+            //create widgets
             btnCalc = new Button();
             btnCalc.Location = new Point(700, 510);
             btnCalc.Size = new Size(70, 40);
-            btnCalc.Text = "Hohoho";
-
-            this.Controls.Add(btnCalc);
+            btnCalc.Text = "Draw Graph";
             btnCalc.Click += BtnCalc_Click;
+            this.Controls.Add(btnCalc);
 
+            //turn form into a graphics surface
+            g = this.CreateGraphics();
+        }
+
+        //EVENTS
+        private void MyForm_Shown(object sender, EventArgs e)
+        {
+            g.DrawLine(greyPen, new Point(150, 280), new Point(650, 280));
+            g.DrawLine(greyPen, new Point(400, 20), new Point(400, 520));
         }
 
         private void BtnCalc_Click(object sender, EventArgs e)
         {
-            //Pushing the button will draw on the graphics
-            g.DrawLine(new Pen(Color.Black), new Point(x1,y1), new Point(x2, y2));
-            x1 += 5;
-            x2 -= 5;
-        }
+            //Pushing the button will draw a graph on 
+            Point zeroPoint = new Point(400, 280);
 
-        private void InitGraphics()
-        {
-            //TURN FORM INTO GRAPHICS
-            g = this.CreateGraphics();
-            
+            for (int x = -100; x < 100; x++)
+            {
+                int y = (int)Math.Pow(x, 2) - (4 * x) + 2;
+                g.DrawLine(blackPen,new Point(zeroPoint.X +x, zeroPoint.Y ), new Point(zeroPoint.X +x, zeroPoint.Y - y));
+            }
         }
-
 
     }
 }
