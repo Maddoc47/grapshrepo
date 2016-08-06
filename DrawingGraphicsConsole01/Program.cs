d0d8c0026cfceea73c0afabf5809d0bef5f0f1d0
c0re <maddoc46@yahoo.com>
2016-08-06 15:40:43 +0200

    Initial Commit

---
+++ DrawingGraphicsConsole01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace DrawingGraphicsConsole01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Form frmSurface = new Form();
+            frmSurface.Text = "Can we draw on this?";
+            Application.Run(frmSurface);            
+        }
+    }
+}
