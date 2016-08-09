e6ecea94a6b88070ecafdeea42cf5f9fdab8e52b
c0re <maddoc46@yahoo.com>
2016-08-09 15:17:01 +0200

    Input Fields are working

--- DrawingGraphicsConsole01/MyForm.cs
+++ DrawingGraphicsConsole01/MyForm.cs
@@ -17,6 +17,11 @@ namespace DrawingGraphicsConsole01
         Pen greyPen = new Pen(Color.Gray);
         Pen redPen = new Pen(Color.Red);
 
+        Label lblA, lblB, lblC, lblD;
+        TextBox txtA, txtB, txtC, txtD;
+        GroupBox pnl;
+
+
         //CONSTRUCTOR
         private MyForm()
         {
@@ -51,6 +56,59 @@ namespace DrawingGraphicsConsole01
             btnClear.Click += BtnClear_Click;
             this.Controls.Add(btnClear);
 
+            //create labels en textfields for user input
+            pnl = new GroupBox();
+            pnl.Size = new Size(200, 180);
+            pnl.Location = new Point(20, 350);
+            pnl.Text = "Input Panel";
+
+            lblA = new Label();
+            lblA.Location = new Point(10, 20);
+            lblA.Size = new Size(70, 20);
+            lblA.Text = "Variable a:";
+            pnl.Controls.Add(lblA);
+
+            txtA = new TextBox();
+            txtA.Location = new Point(90, 20);
+            txtA.Focus();
+            pnl.Controls.Add(txtA);
+
+            lblB = new Label();
+            lblB.Location = new Point(10, 60);
+            lblB.Size = new Size(70, 20);
+            lblB.Text = "Variable b:";
+            pnl.Controls.Add(lblB);
+
+            txtB = new TextBox();
+            txtB.Location = new Point(90, 60);
+            txtB.Focus();
+            pnl.Controls.Add(txtB);
+
+            lblC = new Label();
+            lblC.Location = new Point(10, 100);
+            lblC.Size = new Size(70, 20);
+            lblC.Text = "Variable c:";
+            pnl.Controls.Add(lblC);
+
+            txtC = new TextBox();
+            txtC.Location = new Point(90, 100);
+            txtC.Focus();
+            pnl.Controls.Add(txtC);
+            
+            lblD = new Label();
+            lblD.Location = new Point(10, 140);
+            lblD.Size = new Size(70, 20);
+            lblD.Text = "Variable d:";
+            pnl.Controls.Add(lblD);
+
+            txtD = new TextBox();
+            txtD.Location = new Point(90, 140);
+            txtD.Focus();
+            pnl.Controls.Add(txtD);
+
+            
+            this.Controls.Add(pnl);
+
             //turn form into a graphics surface
             g = this.CreateGraphics();
         }
@@ -69,13 +127,33 @@ namespace DrawingGraphicsConsole01
         }
         private void BtnCalc_Click(object sender, EventArgs e)
         {
-            //Pushing the button will draw a graph on 
-            Point zeroPoint = new Point(400, 280);
+            //Pushing the button will draw a graph 
 
-            for (int x = -100; x < 100; x++)
+            //get numeric values from textbox
+            double a, b, c, d;
+            a = b = c = d = 0;
+
+            try
             {
-                int y = (int)(2 * Math.Pow(x, 2)) + (5 * x) + 20;
-                g.DrawLine(redPen,new Point(zeroPoint.X +x, zeroPoint.Y - y + 3), new Point(zeroPoint.X +x, zeroPoint.Y - y));                
+                a = Double.Parse(txtA.Text);
+                b = Double.Parse(txtB.Text);
+                c = Double.Parse(txtC.Text);
+                d = Double.Parse(txtD.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occured! Please try again!\n " + ex.Message);
+            }
+
+
+            PointF zeroPoint = new PointF(400f, 280f);
+           
+        
+
+            for (float x = -200; x < 100; x++)
+            {
+                float y = (float)(((a * Math.Pow(x, 3)) + (b * Math.Pow(x, 2)) + (c * x) + d)/50);
+                g.DrawLine(redPen,new PointF(zeroPoint.X + x, zeroPoint.Y - y + 3), new PointF(zeroPoint.X +x, zeroPoint.Y - y));                
             }
         }
 
